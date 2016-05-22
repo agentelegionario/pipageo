155dbffb896c6faccc41bcb54d3d5115b8634068
Agentelegionario <alcidesfrancisco@gmail.com>
2016-05-22 10:26:07 -0300

    mudei o nome do arquivo userController para paController

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //--------------------------------Definindo minhas variáveis a ser utilizadas-----------------------------------------------
 var app = require('./config/app_config.js');
 
-var userController = require('./controller/userController.js');
+var paController = require('./controller/paController.js');
 
 var validator = require('validator');
 
@@ -28,7 +28,7 @@ app.get('/', function (req, res) {
 
 app.get('/pa', function (req, res) {
    
-   userController.list(function(resp) {
+   paController.list(function(resp) {
        res.jsonp(resp);
    });
     
@@ -43,7 +43,7 @@ app.get('/pa/:placa', function (req, res) {
    
     var placa = validator.trim(validator.escape(req.param('placa')));
     
-    userController.pa(placa, function(resp) {
+    paController.pa(placa, function(resp) {
         res.jsonp(resp);
     });
     
@@ -62,7 +62,7 @@ app.post('/pa', function (req, res) {
     var geolocaPA = validator.trim(validator.escape(req.param('geolocaPA')));
     var geolocaPAGET = validator.trim(validator.escape(req.param('geolocaPAGET')));
     
-    userController.save(placa, geolocaPA, geolocaPAGET, function(resp) {
+    paController.save(placa, geolocaPA, geolocaPAGET, function(resp) {
         
         res.jsonp(resp);
     });
@@ -84,7 +84,7 @@ app.put('/pa', function (req, res) {
     var email = validator.trim(validator.escape(req.param('email')));
     var password = validator.trim(validator.escape(req.param('password')));
     
-    userController.update(id, fullname, email, password, function(resp) {
+    paController.update(id, fullname, email, password, function(resp) {
         res.jsonp(resp);
     });
 });
@@ -98,7 +98,7 @@ app.delete('/pa/:id', function (req, res) {
    
     var id = validator.trim(validator.escape(req.param('id')));
     
-    userController.delete(id, function(resp){
+    paController.delete(id, function(resp){
         res.jsonp(resp);
     });
     
